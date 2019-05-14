b1fd40591a2280059f2ed1865d9cee1900f9f76f
Pawel Haladyj <pawel@haladyj.pl>
2019-05-14 18:46:44 +0200

    UserController .findAll

--- src/main/java/pl/haladyj/notifications/User/UserController.java
+++ src/main/java/pl/haladyj/notifications/User/UserController.java
@@ -1,13 +1,27 @@
 package pl.haladyj.notifications.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping
+    public List<UserDTO> findAll(){
+        return userService.findAllUsers();
+    }
+
+
 }
