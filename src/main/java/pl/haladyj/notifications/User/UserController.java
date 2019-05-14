3ea003b68fc88c20a45e806577707cd6adecc3d2
Pawel Haladyj <pawel@haladyj.pl>
2019-05-14 19:15:02 +0200

    UserController .createUser()

--- src/main/java/pl/haladyj/notifications/User/UserController.java
+++ src/main/java/pl/haladyj/notifications/User/UserController.java
@@ -1,11 +1,10 @@
 package pl.haladyj.notifications.User;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,10 +13,12 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final UserConverter userConverter;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserConverter userConverter) {
         this.userService = userService;
+        this.userConverter = userConverter;
     }
 
     @GetMapping
@@ -29,5 +30,17 @@ public class UserController {
     public ResponseEntity<UserDTO> findById(@PathVariable Long id){
         return ResponseEntity.ok().body(userService.findById(id));
     }
+
+    @PostMapping("/create")
+    public ResponseEntity<UserDTO> createUser (@RequestParam("map") String createdUser){
+        Gson gson =new Gson();
+        User user = gson.fromJson(createdUser, User.class);
+
+        userService.createUser(userConverter.toDTO(user));
+
+        return ResponseEntity.ok().body(userConverter.toDTO(user));
+    }
+
+
 
 }
